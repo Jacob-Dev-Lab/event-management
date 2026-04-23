dfc6f1d267359b3d0b82a4dfaa98e0e8ae1948a2
Jacob Folorunsho <folorunshojacob05@gmail.com>
2026-04-23 18:16:09 +0100

    feat: implement Item module with full CRUD operations for rental items - [implemented Item entity, sqlserver express database setup, ef core migration, CRUD operations]

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RentalService.Models;
+
+namespace RentalService.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Item> Items { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    }
+}
---
+++ Views/Items/index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RentalService.Views.Items
+{
+    public class indexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
